58e21c02be82a4d1159780ec90539c625ee4698f
りんご飴196 <kazumac41@gmail.com>
2023-10-12 23:18:06 +0900

    金床追加

---
+++ src/main/kotlin/com/github/Ringoame196/Anvil.kt
@@ -0,0 +1,126 @@
+package com.github.Ringoame196
+
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryView
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.EnchantmentStorageMeta
+
+class Anvil {
+    fun open(player: Player) {
+        if (Job().get(player) != "${ChatColor.GRAY}鍛冶屋") {
+            Player().errorMessage(player, "金床は鍛冶屋以外使用できません")
+            return
+        }
+        val gui = Bukkit.createInventory(null, 9, "${ChatColor.YELLOW}カスタム金床")
+        for (i in 0..8) {
+            gui.setItem(i, Item().make(Material.RED_STAINED_GLASS_PANE, " ", null, null))
+        }
+        gui.setItem(2, Item().make(Material.AIR, " ", null, null))
+        gui.setItem(4, Item().make(Material.AIR, " ", null, null))
+        gui.setItem(7, Item().make(Material.ANVIL, "${ChatColor.YELLOW}合成", null, null))
+        player.openInventory(gui)
+    }
+    fun click(player: Player, item: ItemStack, e: InventoryClickEvent) {
+        player.playSound(player, Sound.UI_BUTTON_CLICK, 1f, 1f)
+        when (item.type) {
+            Material.RED_STAINED_GLASS_PANE -> e.isCancelled = true
+            Material.ANVIL -> {
+                e.isCancelled = true
+                synthesis(player, player.openInventory.topInventory)
+            }
+        }
+    }
+    fun close(gui: InventoryView, player: Player) {
+        if (gui.getItem(2) != null) {
+            player.inventory.addItem(gui.getItem(2))
+        }
+        if (gui.getItem(4) != null) {
+            player.inventory.addItem(gui.getItem(4))
+        }
+    }
+    private fun overEnchant(item: ItemStack): Boolean {
+        var allLevel = 0
+        for ((enchant, level) in item.itemMeta?.enchants ?: return false) {
+            allLevel += level
+        }
+        return allLevel > 10
+    }
+    private fun synthesis(player: Player, gui: Inventory) {
+        val syntheticItems1 = gui.getItem(2) ?: return
+        val syntheticItems2 = gui.getItem(4) ?: return
+        if (syntheticItems2.itemMeta?.displayName != "" && syntheticItems2.itemMeta?.displayName != "${ChatColor.YELLOW}修理キット") {
+            Player().errorMessage(player, "右側に名前付きのアイテムを設置することはできません")
+            return
+        }
+        var completedItem = syntheticItems1.clone()
+        if (syntheticItems1.type == syntheticItems2.type) {
+            if (!enchantItemCheck(syntheticItems1.type)) {
+                return
+            }
+            completedItem = enchant(syntheticItems2, completedItem)
+            completedItem = durability(syntheticItems2, completedItem)
+        } else if (syntheticItems2.type == Material.ENCHANTED_BOOK) {
+            completedItem = enchantBook(completedItem, syntheticItems2.itemMeta as EnchantmentStorageMeta)
+        } else if (syntheticItems2.itemMeta!!.displayName == "${ChatColor.YELLOW}修理キット") {
+            if (syntheticItems2.durability.toInt() > syntheticItems2.amount) {
+                Player().errorMessage(player, "修理キットが多すぎます")
+                return
+            }
+            completedItem.durability = (completedItem.durability - syntheticItems2.amount).toShort()
+        }
+        if (overEnchant(completedItem)) {
+            Player().errorMessage(player, "オーバーエンチャント")
+            return
+        }
+        complete(player, completedItem, gui)
+    }
+    private fun complete(player: Player, completedItem: ItemStack, gui: Inventory) {
+        player.playSound(player, Sound.BLOCK_ANVIL_USE, 1f, 1f)
+        player.inventory.addItem(completedItem)
+        gui.setItem(2, Item().make(Material.AIR, " ", null, null))
+        gui.setItem(4, Item().make(Material.AIR, " ", null, null))
+    }
+    private fun enchant(beforeItem: ItemStack, afterItem: ItemStack): ItemStack {
+        for ((enchant, level) in beforeItem.itemMeta?.enchants ?: return afterItem) {
+            afterItem.addUnsafeEnchantment(enchant, level)
+        }
+        return afterItem
+    }
+    private fun enchantBook(afterItem: ItemStack, meta: EnchantmentStorageMeta): ItemStack {
+        for ((enchantment, level) in meta.storedEnchants) {
+            afterItem.addUnsafeEnchantment(enchantment, level)
+        }
+        return afterItem
+    }
+    private fun durability(beforeItem: ItemStack, afterItem: ItemStack): ItemStack {
+        val maxdurability = beforeItem.type.maxDurability
+        val durability = (afterItem.durability - (maxdurability - beforeItem.durability)).toShort()
+        afterItem.durability = if (durability >= 0) { durability } else { 0 }
+        return afterItem
+    }
+    private fun enchantItemCheck(material: Material): Boolean {
+        val allowedItems = mutableListOf(
+            "_SWORD",
+            "_AXE",
+            "_PICKAXE",
+            "_SHOVEL",
+            "_HOE",
+            "SHIELD",
+            "ENCHANTED_BOOK",
+            "_HELMET",
+            "_CHESTPLATE",
+            "_LEGGINGS",
+            "_BOOTS",
+        )
+        for (id in allowedItems) {
+            if (material.toString().contains(id)) { return true }
+        }
+        return false
+    }
+}
--- src/main/kotlin/com/github/Ringoame196/Job.kt
+++ src/main/kotlin/com/github/Ringoame196/Job.kt
@@ -1,17 +1,18 @@
 package com.github.Ringoame196
 
 import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.Particle
 import org.bukkit.Sound
 import org.bukkit.entity.Player
 
 class Job {
-    val job = mutableListOf<String>("無職", "${ChatColor.YELLOW}料理人", "${ChatColor.GOLD}ハンター", "${ChatColor.GRAY}鍛冶屋")
+    val job = mutableListOf("無職", "${ChatColor.YELLOW}料理人", "${ChatColor.GOLD}ハンター", "${ChatColor.GRAY}鍛冶屋")
     fun get(player: Player): String {
-        return job[Scoreboard().getValue("job", player.name)]
+        return job[Scoreboard().getValue("job", player.uniqueId.toString())]
     }
     fun prefix(player: Player) {
-        val displayName = "[${get(player)}${ChatColor.WHITE}]${player.name}" + if (player.isOp) { "${ChatColor.AQUA}@運営" } else {}
+        val displayName = "[${get(player)}${ChatColor.WHITE}]${player.name}" + if (player.isOp) { "${ChatColor.AQUA}@運営" } else { "" }
         player.setDisplayName(displayName)
         player.setPlayerListName(displayName)
     }
@@ -20,13 +21,33 @@ class Job {
         val item = player.inventory.itemInMainHand
         item.amount = item.amount - 1
         for (i in 0 until job.size) {
-            if (job.get(i) != jobName.toString()) { continue }
-            Scoreboard().set("job", player.name, i)
+            if (job.get(i) != jobName) { continue }
+            Scoreboard().set("job", player.uniqueId.toString(), i)
             prefix(player)
             player.sendMessage("${jobName}に就職しました")
             player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 1f, 1f)
             player.world.spawnParticle(Particle.EXPLOSION_HUGE, player.location, 1)
             player.inventory.setItemInMainHand(item)
         }
+    }
+    fun tool(): List<Material> {
+        val list = mutableListOf<Material>(
+            Material.IRON_SWORD,
+            Material.DIAMOND_SWORD,
+            Material.NETHERITE_SWORD,
+            Material.IRON_PICKAXE,
+            Material.DIAMOND_PICKAXE,
+            Material.NETHERITE_PICKAXE,
+            Material.IRON_AXE,
+            Material.DIAMOND_AXE,
+            Material.NETHERITE_AXE,
+            Material.IRON_SHOVEL,
+            Material.DIAMOND_SHOVEL,
+            Material.NETHERITE_SHOVEL,
+            Material.IRON_HOE,
+            Material.DIAMOND_HOE,
+            Material.NETHERITE_HOE,
+        )
+        return list
     }
 }
